500c8d656ce8024e76639f336d1fd86b28f13b51
Dominik H <dcdominoes@gmail.com>
2022-10-28 12:58:27 +0100

    Changed preventdefault order

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import getDirection from './helpers/getDirection.js';
 
 window.Game = new Game(document.getElementById('board'));
 window.addEventListener("keydown", (e) => {
+  window.Game.changeDiirection(getDirection(e.key));
   e.preventDefault();
-  return window.Game.changeDirection(getDirection(e.key));
 });
 window.addEventListener("load", (e) => setInterval(() => window.Game.gameLoop(), 30));
