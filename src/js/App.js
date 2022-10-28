b8682017a52623effe43526160dbe1749bd78b49
Dominik H <dcdominoes@gmail.com>
2022-10-28 15:06:06 +0100

    Fix typo

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import getDirection from './helpers/getDirection.js';
 
 window.Game = new Game(document.getElementById('board'));
 window.addEventListener("keydown", (e) => {
-  window.Game.changeDiirection(getDirection(e.key));
+  window.Game.changeDirection(getDirection(e.key));
   e.preventDefault();
 });
 window.addEventListener("load", (e) => setInterval(() => window.Game.gameLoop(), 30));
